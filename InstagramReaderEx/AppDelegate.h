f2351ec01fda091eba88ccf1a12db399dea166c8
raytjwen <ray.tjwen@yahoo.com.tw>
2015-06-19 15:40:49 +0800

    Initial Commit

---
+++ InstagramReaderEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramReaderEx
+//
+//  Created by AlleyOops on 2015/6/19.
+//  Copyright (c) 2015年 AlleyOops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstagramReaderEx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramReaderEx
+//
+//  Created by AlleyOops on 2015/6/19.
+//  Copyright (c) 2015年 AlleyOops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
